469b18cf4783f7e9c059042571325d97c65c6cfe
Evokoo <evanjohncooper@gmail.com>
2023-01-22 00:47:56 +1100

    Problem 050

---
+++ 000/getPermutations.ts
@@ -0,0 +1,29 @@
+export default function getPermutations(input: number[]): string[] {
+	const output: string[] = [];
+
+	const swap = (arr: number[], a: number, b: number): number[] => {
+		return ([arr[a], arr[b]] = [arr[b], arr[a]]);
+	};
+
+	const generate = (len: number, arr: number[]) => {
+		if (len === 1) {
+			output.push(arr.slice().join(""));
+			return;
+		}
+
+		generate(len - 1, arr);
+
+		for (let i = 0; i < len - 1; i++) {
+			if (len % 2 === 0) {
+				swap(arr, i, len - 1);
+			} else {
+				swap(arr, 0, len - 1);
+			}
+			generate(len - 1, arr);
+		}
+	};
+
+	generate(input.length, input.slice());
+
+	return output;
+}
