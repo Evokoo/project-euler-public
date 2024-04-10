d6ad9bf22bb599ae5e0a7efa373dcf7c428d52da
Evokoo <evanjohncooper@gmail.com>
2024-04-11 03:03:06 +1000

    Problem 062 complete

--- 062/062.ts
+++ 062/062.ts
@@ -7,3 +7,32 @@ In fact, 41063625 is the smallest cube which has exactly three permutations of i
 
 Find the smallest cube for which exactly five permutations of its digits are cube.
 */
+
+function generateCubeNumbers(min: number, max: number): Map<string, number[]> {
+	const cubeNumbers: Map<string, number[]> = new Map();
+
+	for (let i = min; i <= max; i++) {
+		const cubeNumber = i ** 3;
+		const n = [...String(cubeNumber)].sort().join("");
+
+		if (n.length >= 8) {
+			cubeNumbers.set(n, [...(cubeNumbers.get(n) ?? []), i]);
+		}
+	}
+
+	return cubeNumbers;
+}
+
+export function findSmallestValidCubeNumber(target: number): number {
+	const cubeNumbers = generateCubeNumbers(1, 10_000);
+
+	for (let [_, permutations] of cubeNumbers) {
+		if (permutations.length === target) {
+			return Math.min(...permutations) ** 3;
+		}
+	}
+
+	throw Error("Cube number not found - Try increasing the max range");
+}
+
+console.log(findSmallestValidCubeNumber(5));
