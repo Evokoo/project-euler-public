3c39cfca48c21419fe2927d58473ed14d575ad4d
Evokoo <evanjohncooper@gmail.com>
2024-04-12 00:34:12 +1000

    Problem 079 complete

--- 079/079.ts
+++ 079/079.ts
@@ -8,3 +8,6 @@ The text file, keylog.txt, contains fifty successful login attempts.
 
 Given that the three characters are always asked for in order, analyse the file so as to determine the shortest possible secret passcode of unknown length.
 */
+
+// Solved by hand
+// 73162890
