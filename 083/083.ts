8c440ad8811759fd2e553c27e3717e847b7224ce
Evokoo <evanjohncooper@gmail.com>
2024-04-15 02:50:11 +1000

    Problem 083 WIP

--- 083/083.ts
+++ 083/083.ts
@@ -12,3 +12,36 @@ The minimal path sum in the 5 by 5 matrix below, the minimal path sum from the t
 
 Find the minimal path sum from the top left to the bottom right by moving left, right, up, and down in matrix.txt, a 31K text file containing an 80 by 80 matrix.
  */
+
+//Imports
+import readTextFile from "../000/readTextFile";
+
+//Types
+type Point = { x: number; y: number };
+type Path = Point & { total: number };
+type Matrix = number[][];
+
+function getMatrix(filename: string): number[][] {
+	const input = readTextFile(filename);
+	return input.split("\n").map((row) => row.split(",").map(Number));
+}
+function getNeighbours(point: Point, size: number) {
+	const neighbours: Point[] = [];
+
+	for (let [nx, ny] of [
+		[0, -1],
+		[0, 1],
+		[1, 0],
+		[-1, 0],
+	]) {
+		const [x, y] = [point.x + nx, point.y + ny];
+
+		if (x < 0 || x > size || y < 0 || y > size) {
+			continue;
+		} else {
+			neighbours.push({ x, y });
+		}
+	}
+
+	return neighbours;
+}
