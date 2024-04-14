36e6fc19f3fabc1aca164520667e0ded92f6057e
Evokoo <evanjohncooper@gmail.com>
2024-04-15 02:21:08 +1000

    Problem 82 complete

--- 082/082.ts
+++ 082/082.ts
@@ -12,3 +12,94 @@ The minimal path sum in the 5 by 5 matrix below, by starting in any cell in the 
 
 Find the minimal path sum from the top left to the bottom right by only moving right and down in matrix.txt, a 31K text file containing an 80 by 80 matrix.
  */
+
+//Imports
+import readTextFile from "../000/readTextFile";
+
+//Types
+type Point = { x: number; y: number };
+type Path = Point & { total: number };
+type Matrix = number[][];
+
+function getMatrix(filename: string): number[][] {
+	const input = readTextFile(filename);
+	return input.split("\n").map((row) => row.split(",").map(Number));
+}
+function getNeighbours(point: Point, size: number) {
+	const neighbours: Point[] = [];
+
+	for (let [nx, ny] of [
+		[0, -1],
+		[0, 1],
+		[1, 0],
+	]) {
+		const [x, y] = [point.x + nx, point.y + ny];
+
+		if (x < 0 || x > size || y < 0 || y > size) {
+			continue;
+		} else {
+			neighbours.push({ x, y });
+		}
+	}
+
+	return neighbours;
+}
+function getPathSum(start: Point, minSum: number, matrix: Matrix) {
+	const size: number = matrix.length - 1;
+	const queue: Path[] = [
+		{
+			x: start.x,
+			y: start.y,
+			total: matrix[start.y][start.x],
+		},
+	];
+	const seen: Set<string> = new Set();
+
+	while (queue.length) {
+		const current: Path = queue.shift()!;
+		const coord: string = `${current.x},${current.y}`;
+
+		if (seen.has(coord)) {
+			continue;
+		} else {
+			seen.add(coord);
+		}
+
+		if (current.total > minSum) {
+			return Infinity;
+		}
+
+		if (current.x === size) {
+			return current.total;
+		} else {
+			for (let { x, y } of getNeighbours(
+				{ x: current.x, y: current.y },
+				size
+			)) {
+				queue.push({
+					x,
+					y,
+					total: current.total + matrix[y][x],
+				});
+			}
+		}
+
+		queue.sort((a, b) => a.total - b.total);
+	}
+
+	throw Error("Something went wrong");
+}
+function findMinimalSumPath(filename: string) {
+	const matrix: number[][] = getMatrix(filename);
+
+	let minTotal: number = Infinity;
+
+	for (let y = 0; y < matrix.length; y++) {
+		const pathSum = getPathSum({ x: 0, y }, minTotal, matrix);
+		minTotal = Math.min(pathSum, minTotal);
+	}
+
+	return minTotal;
+}
+
+console.log(findMinimalSumPath("matrix"));
