92b0578b3940d814c4f7202af977786ab7de16a7
Evokoo <evanjohncooper@gmail.com>
2024-04-21 02:13:04 +1000

    Problem 061 test added

--- projectEuler.test.ts
+++ projectEuler.test.ts
@@ -1,6 +1,7 @@
 import { test, expect, describe } from "bun:test";
 import _051 from "./051/051";
 import _053 from "./053/053";
+import _061 from "./061/061";
 
 describe("Project Euler Tests", () => {
 	test("Problem 51", () => {
@@ -8,5 +9,8 @@ describe("Project Euler Tests", () => {
 	});
 	test("Problem 53", () => {
 		expect(_053(1_000_000)).toBe(4075);
+	});
+	test("Problem 61", () => {
+		expect(_061()).toBe(28684);
 	});
 });
