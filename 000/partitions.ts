c15f47dcf0a482cb839d3f56eab4363dc13af689
Evokoo <evanjohncooper@gmail.com>
2024-04-28 01:30:05 +1000

    Partition help function added

---
+++ 000/partitions.ts
@@ -0,0 +1,13 @@
+export default function partitions(length: number): number[] {
+	const partitions = Array.from({ length: length + 1 }, () => 0);
+
+	partitions[0] = 1;
+
+	for (let i = 1; i <= length; i++) {
+		for (let j = i; j <= length; j++) {
+			partitions[j] += partitions[j - i];
+		}
+	}
+
+	return partitions;
+}
