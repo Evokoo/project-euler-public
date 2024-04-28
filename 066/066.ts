58539c2cfc9095b69660217725f4affb772516b2
Evokoo <evanjohncooper@gmail.com>
2024-04-29 02:01:12 +1000

    Problem 066 WIP

--- 066/066.ts
+++ 066/066.ts
@@ -4,3 +4,30 @@
 /*
     See link for description
 */
+
+function solve(d: number, x: number, y: number): number {
+	return x ** 2 - d * y ** 2;
+}
+
+let maxX = 0;
+
+D: for (let d = 2; d <= 100; d++) {
+	if (Number.isInteger(Math.sqrt(d))) continue;
+
+	for (let y = 1; true; y++) {
+		const limitX = d * y ** 2 + 1;
+
+		for (let x = 1; x < limitX; x++) {
+			console.log({ d, x, y });
+
+			const solution = solve(d, x, y);
+
+			if (solution === 1) {
+				maxX = Math.max(x, maxX);
+				continue D;
+			}
+		}
+	}
+}
+
+console.log(maxX);
