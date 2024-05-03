16d4094d1b4188890c30c4ad2b79d2bc035f6b5e
Evokoo <evanjohncooper@gmail.com>
2024-05-04 01:32:56 +1000

    Refactor: Factorial function is now generic

--- 000/factorial.ts
+++ 000/factorial.ts
@@ -1,4 +1,19 @@
-export default function factorial(n: number): number {
-	if (n === 0) return 1;
-	return n * factorial(n - 1);
+/**
+ * Calculates the factorial of a number or a bigint.
+ * @template T - The type of the input. Must extend `number` or `bigint`.
+ * @param {T} n - The number or bigint for which to calculate the factorial.
+ * @returns {T} The factorial of the input value.
+ * @throws {TypeError} If `n` is not a number or a bigint.
+ */
+
+export default function factorial<T extends number | bigint>(n: T): T {
+	if (typeof n === "number") {
+		if (n === 0) return 1 as T;
+		return (n * factorial(n - 1)) as T;
+	} else if (typeof n === "bigint") {
+		if (n === 0n) return 1n as T;
+		return (n * factorial(n - 1n)) as T;
+	} else {
+		throw TypeError("n is an invalid type");
+	}
 }
