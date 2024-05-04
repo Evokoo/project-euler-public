bd355e3b75536a9170265ec91942efa57a5a2b94
Evokoo <evanjohncooper@gmail.com>
2024-05-05 01:28:39 +1000

    Refactor: Problem 023 to Typescript

---
+++ 023/023.ts
@@ -0,0 +1,37 @@
+// Non-Abundant Sums - Problem 023
+// https://projecteuler.net/problem=23
+
+/*
+    A perfect number is a number for which the sum of its proper divisors is exactly equal to the number. For example, the sum of the proper divisors of 28 would be 1 + 2 + 4 + 7 + 14 = 28, which means that 28 is a perfect number.
+
+    A number n is called deficient if the sum of its proper divisors is less than n and it is called abundant if this sum exceeds n.
+
+    As 12 is the smallest abundant number, 1 + 2 + 3 + 4 + 6 = 16, the smallest number that can be written as the sum of two abundant numbers is 24. By mathematical analysis, it can be shown that all integers greater than 28123 can be written as the sum of two abundant numbers. However, this upper limit cannot be reduced any further by analysis even though it is known that the greatest number that cannot be expressed as the sum of two abundant numbers is less than this limit.
+
+    Find the sum of all the positive integers which cannot be written as the sum of two abundant numbers.
+*/
+
+import Arithmetic from "../000/Arithmetic";
+import Divisors from "../000/Divisors";
+
+function getAbundantSum(limit: number) {
+	const postiveIntegers: Set<number> = new Set(
+		Arithmetic.generateRange(1, limit)
+	);
+
+	const abundantNumbers: number[] = [];
+
+	for (let i = 12; i <= limit; i++) {
+		if (Divisors.isAbundant(i)) {
+			abundantNumbers.push(i);
+
+			for (let number of abundantNumbers) {
+				postiveIntegers.delete(i + number);
+			}
+		}
+	}
+
+	return Arithmetic.sum([...postiveIntegers]);
+}
+
+console.log(getAbundantSum(28123));
