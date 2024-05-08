090f2b0578007709745cc4109b06d69a49075e42
Evokoo <evanjohncooper@gmail.com>
2024-05-09 03:30:45 +1000

    Added generic rotateInput method

--- 000/Misc.ts
+++ 000/Misc.ts
@@ -70,4 +70,71 @@ export default class Misc {
 
 		return [...output];
 	}
+
+	/**
+	 * Rotates the input `string` or `number` in the specified direction for a certain number of steps.
+	 * If steps are not provided, rotates infinitely until the sequence repeats.
+	 *
+	 * @template T - Type of the input (`string` or `number`).
+	 * @param {T} input - The input to be rotated.
+	 * @param {string} direction - The direction of rotation. "L" for left and "R" for right.
+	 * @param {number} [steps=Infinity] - The number of steps to rotate. Defaults to Infinity.
+	 * @throws {RangeError} - If the input number is negative.
+	 * @throws {TypeError} - If the direction is neither "L" nor "R".
+	 * @returns {T[] | T} Array of rotated inputs if finite steps provided, or last rotated input if steps are infinite.
+	 * @example
+	 *
+	 * Misc.rotateInput("Test", "R") returns ["Test", "tTes", "stTe", "estT"]
+	 * Misc.rotateInput("Test", "L") returns ["Test", "estT", "stTe", "tTes"]
+	 * Misc.rotateInput("Test", "R", 2) returns "stTe"
+	 *
+	 * Misc.rotateInput("1234", "R") returns [1234, 4123, 3412, 2341]
+	 * Misc.rotateInput("1234", "L") returns [1234, 4123, 3412, 2341]
+	 * Misc.rotateInput("1234", "R", 2) returns 3412
+	 */
+	static rotateInput<T extends string | number>(
+		input: T,
+		direction: string,
+		steps: number = Infinity
+	): T[] | T {
+		if (typeof input === "number" && input < 0) {
+			throw RangeError("Input number must be >= 0");
+		}
+
+		if (typeof input === "string" && input.length === 0) {
+			return "" as T;
+		}
+
+		if (direction !== "R" && direction !== "L") {
+			throw TypeError("Direction must be L (left) or R (right)");
+		}
+
+		const rotations: Set<T> = new Set([input]);
+		const characters: string[] = [...String(input)];
+
+		for (let step = 0; step < steps; step++) {
+			if (direction === "R") {
+				characters.push(characters.shift()!);
+			} else {
+				characters.unshift(characters.pop()!);
+			}
+
+			const rotation =
+				typeof input === "string"
+					? (characters.join("") as T)
+					: (Number(characters.join("")) as T);
+
+			if (rotations.has(rotation)) {
+				break;
+			} else {
+				rotations.add(rotation);
+			}
+		}
+
+		if (steps === Infinity) {
+			return [...rotations];
+		} else {
+			return [...rotations].at(-1) as T;
+		}
+	}
 }
