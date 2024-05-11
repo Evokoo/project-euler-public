0022532055997c05349df53f12739d6efe67cb4d
Evokoo <evanjohncooper@gmail.com>
2024-05-12 02:27:20 +1000

    Refactor: Problem 046

--- 046/046.ts
+++ 046/046.ts
@@ -2,43 +2,35 @@
 // https://projecteuler.net/problem=46
 
 /*
-It was proposed by Christian Goldbach that every odd composite number can be written as the sum of a prime and twice a square.
+	It was proposed by Christian Goldbach that every odd composite number can be written as the sum of a prime and twice a square.
 
-9 = 7 + 2×12
-15 = 7 + 2×22
-21 = 3 + 2×32
-25 = 7 + 2×32
-27 = 19 + 2×22
-33 = 31 + 2×12
+	9 = 7 + 2×12
+	15 = 7 + 2×22
+	21 = 3 + 2×32
+	25 = 7 + 2×32
+	27 = 19 + 2×22
+	33 = 31 + 2×12
 
-It turns out that the conjecture was false.
+	It turns out that the conjecture was false.
 
-What is the smallest odd composite that cannot be written as the sum of a prime and twice a square?
+	What is the smallest odd composite that cannot be written as the sum of a prime and twice a square?
 */
 
-import primeSieve from "../000/primeSieve";
+import Arithmetic from "../000/Arithmetic";
+import Prime from "../000/Prime";
 
-function isComposite(n: number): boolean {
-	let count = 0;
-
-	for (let i = 1; i < n; i++) {
-		if (n % i === 0) count++;
-		if (count > 2) return true;
-	}
-
-	return false;
-}
-function isOdd(n: number): boolean {
-	return n % 2 !== 0;
-}
 function findMinimalGoldbachViolation(limit: number) {
-	const primeNumbers: Set<number> = new Set(primeSieve(2, limit));
+	const primeNumbers: Set<number> = new Set(Prime.generatePrimes(2, limit));
 	const compositeNumbers: Set<number> = new Set();
 	const squareNumbers: Set<number> = new Set();
 
 	for (let i = 1; i < limit; i++) {
-		if (isOdd(i) && isComposite(i)) compositeNumbers.add(i);
-		if (i * i <= limit) squareNumbers.add(i * i);
+		if (Arithmetic.isOdd(i) && Arithmetic.isComposite(i)) {
+			compositeNumbers.add(i);
+		}
+		if (i * i <= limit) {
+			squareNumbers.add(i * i);
+		}
 	}
 
 	for (let prime of primeNumbers) {
