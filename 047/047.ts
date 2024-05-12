a976cc9b015c527a80511d15cb787daae5ec5b96
Evokoo <evanjohncooper@gmail.com>
2024-05-13 01:27:18 +1000

    Refactor: Problem 047

--- 047/047.ts
+++ 047/047.ts
@@ -2,46 +2,34 @@
 // https://projecteuler.net/problem=47
 
 /*
-The first two consecutive numbers to have two distinct prime factors are:
+	The first two consecutive numbers to have two distinct prime factors are:
 
-14 = 2 × 7
-15 = 3 × 5
+	14 = 2 × 7
+	15 = 3 × 5
 
-The first three consecutive numbers to have three distinct prime factors are:
+	The first three consecutive numbers to have three distinct prime factors are:
 
-644 = 2² × 7 × 23
-645 = 3 × 5 × 43
-646 = 2 × 17 × 19.
+	644 = 2² × 7 × 23
+	645 = 3 × 5 × 43
+	646 = 2 × 17 × 19.
 
-Find the first four consecutive integers to have four distinct prime factors each. What is the first of these numbers?
+	Find the first four consecutive integers to have four distinct prime factors each. What is the first of these numbers?
 */
 
-import primeSieve from "../000/primeSieve";
-
-const primes = primeSieve(2, 1_000_000);
-
-function primeFactorsOfN(n: number) {
-	const factors = [];
-
-	for (let prime of primes) {
-		if (prime > n) break;
-		if (n % prime === 0) factors.push(prime);
-	}
-
-	return factors;
-}
+import Prime from "../000/Prime";
 
 function distinctPrimeFactors(target: number) {
 	const result: Set<number> = new Set();
-	let n = 0;
+	const primes: Set<number> = new Set(Prime.generatePrimes(2, 1_000_000));
 
-	while (result.size !== target) {
-		if (primeFactorsOfN(n).length === target) {
+	for (let n = 0; result.size !== target; n++) {
+		const primeFactors: number[] = Prime.primeFactors(n, primes);
+
+		if (primeFactors.length === target) {
 			result.add(n);
 		} else {
 			result.clear();
 		}
-		n++;
 	}
 
 	return [...result][0];
