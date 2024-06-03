79b3d10dab3852bfdd2211d434fa6823c3ce65b0
Evokoo <evanjohncooper@gmail.com>
2024-06-04 02:47:41 +1000

    Problem 098 WIP

--- 098/098.ts
+++ 098/098.ts
@@ -1,8 +1,6 @@
 // Anagramic Squares - Problem 098
 // https://projecteuler.net/problem=98
 
-
-
 /*
 By replacing each of the letters in the word CARE with 1,2,9 and 6 respectively, we form a square number: 1296 = 36^2
 .What is remarkable is that, by using the same digital substitutions, the anagram, RACE, also forms a square number: 9216 = 96 ^ 2. We shall call CARE (and RACE) a square anagram word pair and specify further that leading zeroes are not permitted, neither may a different letter have the same digital value as another letter.
@@ -25,7 +23,7 @@ function isAnagram(a: string | number, b:string | number): boolean{
     return sortedString(a) === sortedString(b);
 }
 function anagramWords(filename: string): Map<string,string[]>{
-    const words: string[] = IO.readFile(filename).split(",");
+    const words: string[] = IO.readFile(filename).match(/[A-Z]+/g) || [];
     const map: Map<string, string[]> = new Map();
     const seen: Set<string> = new Set();
 
@@ -59,4 +57,34 @@ function anagramSquares(): Map<number, number[]>{
     return map;
 }
 
-console.log(anagramSquares())  
+function largestAnagramicSquare(filename: string){
+    const wordAnagrams: Map<string, string[]> = anagramWords(filename);
+    const squareAnagrams: Map<number, number[]> = anagramSquares();
+
+    let largest = 0;
+
+    for(let [keyword, words] of [...wordAnagrams]){
+        
+
+        for(let [keysquare, squares] of [...squareAnagrams]){
+            if(String(keysquare).length !== keyword.length) continue;
+
+            const characters: Record<string, number> = {};
+            const digits = String(keysquare);
+
+            for(let i = 0; i < digits.length; i++){
+                characters[keyword[i]] = +digits[i];
+            }
+
+            let match = words.find((word) => squares.includes(+[...word].map(c => characters[c] ?? "_").join("")));
+
+            if(match){
+                largest = Math.max(largest, keysquare);
+            }  
+        }
+    }
+
+    console.log(squareAnagrams.get(largest));
+}
+
+largestAnagramicSquare("words");
