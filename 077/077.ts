93a86781553eb8d6edf3e6f0d4e1bf8e14c31608
Evokoo <evanjohncooper@gmail.com>
2024-06-08 03:23:18 +1000

    Problem 077 complete

--- 077/077.ts
+++ 077/077.ts
@@ -12,3 +12,32 @@ It is possible to write ten as the sum of primes in exactly five different ways:
 
 What is the first value which can be written as the sum of primes in over five thousand different ways?
 */
+
+import Prime from "../000/Prime";
+
+function countCombinations(target: number, numbers: number[]): number {
+	let dp: number[] = Array.from({ length: target + 1 }, (_) => 0);
+	dp[0] = 1;
+
+	for (let n of numbers) {
+		for (let i = n; i <= target; i++) {
+			dp[i] += dp[i - n];
+		}
+	}
+
+	return dp[target];
+}
+
+function primeSummationCount(target: number) {
+	const primes: number[] = Prime.generatePrimes(2, 100);
+
+	for (let i = 10; true; i++) {
+		const combinations = countCombinations(i, primes);
+
+		if (combinations >= target) {
+			return i;
+		}
+	}
+}
+
+console.log(primeSummationCount(5000));
