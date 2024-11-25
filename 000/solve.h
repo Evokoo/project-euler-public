9b4d0aa8c6aad27f0c5af60668083cad3c4e06f0
Evokoo <evanjohncooper@gmail.com>
2024-11-26 02:21:12 +1100

    Solve header created, contains print_solution function

---
+++ 000/solve.h
@@ -0,0 +1,9 @@
+#define SOLVE
+
+#include <string>
+#include <iostream>
+
+void print_solution(std::string soltuion, std::string problem_id){
+    std::cout << "Project Euler Problem " << problem_id << std::endl;
+    std::cout << "Solution: " << soltuion << std::endl;
+}
